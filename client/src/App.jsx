788851bb7a62abdda5db773524dba61afe33e75d
Lumansito <luhmannm0@gmail.com>
2024-04-16 14:05:58 -0300

    Creadno los primeros componentes de React
    Se creo EmleadoForm, Home NotFound

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import EmpleadoForm from "./pages/EmpleadoForm";
 
-function App(){
-    return (
-        <div>
-            <h1>App</h1>
-        </div>
-    );
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+        <Route path="/empleado/new" element={<EmpleadoForm />} />
+      <Route path="*" element={<NotFound/>} />
+    </Routes>
+  );
 }
 export default App;
---
+++ client/src/pages/EmpleadoForm.jsx
@@ -0,0 +1,8 @@
+function EmpleadoForm() {
+  return (
+    <div>
+      <h1>Empleado Form</h1>
+    </div>
+  );
+}
+export default EmpleadoForm;
