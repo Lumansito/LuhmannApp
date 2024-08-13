b2a1d338ce2c1ef6cce2b6cfa170e78deefc9bfb
Matias Luhmann <139171817+Lumansito@users.noreply.github.com>
2024-08-13 19:46:27 -0300

    Update visitas.controllers.js

--- server/controllers/visitas.controllers.js
+++ server/controllers/visitas.controllers.js
@@ -32,3 +32,21 @@ export const getVisitaById = async (req, res) => {
     }
 };
 
+export const createVisita = async (req, res) => { 
+    try{  
+      const { fecha, horaInicio, horaFin, titulo, nroObra, descripcion} = req.body;      
+      
+      const [contar] = await pool.query("SELECT COUNT(*) FROM visita WHERE nroObra = ? ", [nroObra]);
+      let nroVisita = contar[0]["COUNT(*)"] + 1;
+        const [result] = await pool.query(
+          "INSERT INTO visita (fecha, horaInicio, horaFin,titulo, nroObra, descripcion, nroVisita) VALUES (?, ?, ?, ?, ?, ?)",
+          [fecha, horaInicio,horaFin,titulo, nroObra,descripcion, nroVisita]
+      );
+      if (result.affectedRows === 0) {
+        return res.status(404).json({ message: "Error al crear visita" });
+      }
+      res.json({});
+    }catch(error){
+      console.log(error);
+    }
+}
